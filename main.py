35adc8c5180b051dd84017974ac9a02e594d30f4
Wig C <onlywig@gmail.com>
2017-04-10 01:36:46 +0800

    First initial.

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template('index.html')
+
+@app.route("/products")
+def products():
+    return render_template('products.html')
+
+@app.route("/download")
+def download():
+    return render_template('download.html')
+
+@app.route("/walnut")
+def walnut():
+    return render_template('walnut.html')
+
+@app.route("/overview")
+def overview():
+    return render_template('overview.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True, threaded=True)
